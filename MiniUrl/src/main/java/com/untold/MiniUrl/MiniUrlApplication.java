7c06b0798e20c69fd4113eb92715895928e3dbd0
satya-n015y <satyanayakp001@gmail.com>
2021-05-29 12:13:57 +0530

    committing project structure

---
+++ MiniUrl/src/main/java/com/untold/MiniUrl/MiniUrlApplication.java
@@ -0,0 +1,13 @@
+package com.untold.MiniUrl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniUrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniUrlApplication.class, args);
+	}
+
+}
---
+++ MiniUrl/src/test/java/com/untold/MiniUrl/MiniUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.untold.MiniUrl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniUrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
